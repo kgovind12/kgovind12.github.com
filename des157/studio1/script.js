dcc6660ba742aa39993e7ec57332ab79fce8b010
kgovind12 <kgovind@ucdavis.edu>
2021-01-26 23:53:30 -0800

    added new blog post and studio 1 folder

---
+++ des157/studio1/script.js
@@ -0,0 +1,63 @@
+(function(){
+    'use strict';
+
+    const story = document.querySelector('.story');
+    const form = document.querySelector('form');
+    const formFields = document.querySelectorAll('input');
+    const storyText = document.querySelector('.story p');
+
+    form.addEventListener('submit', function(e){
+        e.preventDefault();
+
+        for(let i=0; i<formFields.length; i++){
+            // if(formFields[i].value == ""){
+            //     alert("Please fill out all the fields!");
+            //     return;
+            // }
+            
+            if (i != 2){
+                formFields[i].value = formFields[i].value.toLowerCase();
+            }
+            else{
+                formFields[i].value = capitalize(formFields[i].value);
+            }
+        }
+
+        storyText.innerHTML = 'Welcome to the Tillydrone rainforest! You can find lots of <span>' + formFields[0].value + '</span> creatures here. We have quiet sloths, colorful macaws, and a few golden bamboo lemurs. If you look <span>' + formFields[1].value + '</span>, you might even catch a river otter or two! Our tour guide, <span>' + formFields[2].value + '</span>, will show you around our diverse rainforest kingdom, and you will also get a chance to <span>' + formFields[3].value + '</span> some rare butterflies. But watch out for the poisonous <span>' + formFields[4].value + '</span>! We hope you have a great time!';
+        
+        story.classList.add('reveal'); 
+        form.classList.add('hide');
+    });
+})();
+
+function removeSpaces(nameString){
+    var endIndex = nameString.length;
+    var result = '';
+    var words = nameString.split(' ');
+    result = words.join(' ');
+    // for(let i=0; i<nameString.length; i++){
+    //     if(nameString[i] == ' ' && nameString[i+1] == ' '){
+    //         result = result + nameString.slice(0,i) + nameString.slice
+    //         console.log(nameString);
+    //     }
+    // }
+    console.log(result);
+    return result;
+}
+
+// function capitalize(nameString){
+//     // removing extra spaces
+//     nameString = nameString.trim();
+//     nameString = removeSpaces(nameString);
+
+//     var words = nameString.split(' ');
+//     var result = '';
+
+//     for(let i=0; i<words.length; i++){
+//         words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+//     }
+
+//     result = words.join(' ');
+
+//     return result;
+// }
