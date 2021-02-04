77bde0b7e427e69994d4f2557da1ea1656c82404
kgovind12 <kgovind@ucdavis.edu>
2021-02-04 10:10:27 -0800

    commented code

--- des157/studio1/script.js
+++ des157/studio1/script.js
@@ -7,9 +7,11 @@
     const formFields = document.querySelectorAll('input');
     const storyText = document.querySelector('.story p');
 
+    // Submit handler for form checks each form field's value.
     form.addEventListener('submit', function(e){
         e.preventDefault();
 
+        // trimming the extra spaces from the word and converting it to lower case (except the name field)
         for(let i=0; i<formFields.length; i++){
             formFields[i].value = formFields[i].value.trim();
             if (i != 2){
@@ -17,6 +19,8 @@
             }
         }
 
+        // Not using template literals because I wanted to underline the fields that the user submitted. 
+        // The third form field has a class of "name" so it can be capitalized.
         storyText.innerHTML = 'Welcome to the Tillydrone rainforest! You can find lots of <span>' + 
         formFields[0].value + '</span> creatures here. We have quiet sloths, colorful macaws, and a few golden bamboo lemurs. If you look <span>' + 
         formFields[1].value + '</span>, you might even spot a river otter or two! Our tour guide, <span class="name">' + 
@@ -24,6 +28,7 @@
         formFields[3].value + '</span> some rare butterflies. But watch out for the poisonous <span>' + 
         formFields[4].value + '</span>! We hope you have a great time!';
         
+        // hiding the form and showing the story
         story.classList.add('reveal'); 
         form.classList.add('hide');
     });
