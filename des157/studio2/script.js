47eddca1f364d08f6d80015b40d75cc33604563d
kgovind12 <kgovind@ucdavis.edu>
2021-02-16 11:59:59 -0800

    descriptions

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -80,25 +80,25 @@
             // set the image description depending on the id of the image clicked
             switch(event.target.id){
                 case 'img1':
-                    imageDesc.innerHTML = 'This photo was taken at the Hare Creek Bridge in Fort Bragg. Under the bridge is a small beach. You can sit here and feel the spray of the water as it laps against the rocks!';
+                    imageDesc.innerHTML = 'This photo was taken at the Hare Creek Bridge in Fort Bragg. Under the bridge is a small beach. I visited during Winter Break last year and loved sitting by the rocks!';
                     break;
                 case 'img2':
                     imageDesc.innerHTML = 'This photo was taken at Lake Elizabeth in Fremont, California. I used to come here all the time as a kid and always loved the sunsets. Here you can see the orange glow of the sun through the trees.';
                     break;
                 case 'img3':
-                    imageDesc.innerHTML = 'This photo was taken at Lake Elizabeth as well, though the bridge is a new addition to the park. A walk along this bridge will give you plenty of time to enjoy the fresh air and the view of the lake!';
+                    imageDesc.innerHTML = 'This photo was taken at Lake Elizabeth as well, though the bridge is a new addition to the park. A walk along this bridge gives me plenty of time to enjoy the fresh air and the view of the lake!';
                     break;
                 case 'img4':
                     imageDesc.innerHTML = 'This photo was taken at Pismo Beach, California, with the help of a pair of sunglasses to give it a pinkish glow. I love the serenity of the ocean at dusk - and the herons that walk along the beach!';
                     break;
                 case 'img5':
-                    imageDesc.innerHTML = 'Taken near Bellagio, Las Vegas, this photo of a bright fountain shows us what the neon capital is all about! Myth, magic, and a hint of red wine will draw you in as you walk along this famous strip.';
+                    imageDesc.innerHTML = 'Taken near Bellagio, Las Vegas, this photo of a bright fountain shows us what the neon capital is all about! I visited during the spring of 2018, and loved the myth and magic of the city!';
                     break;
                 case 'img6':
-                    imageDesc.innerHTML = 'Taken at Las Vegas, Nevada, this photo shows what the heart of the city looks like at night. With the lively music and people, you won\'t ever get bored. It\'s a must visit for lovers of bright city lights!';
+                    imageDesc.innerHTML = 'Taken at Las Vegas, Nevada, this photo shows what the heart of the city looks like at night. With the lively music and people, I never get bored. It\'s a must visit for lovers of bright city lights!';
                     break;
                 case 'img7':
-                    imageDesc.innerHTML = 'Taken at the San Francisco Exploratorium, this photo shows the excited sea on a windy afternoon. Come here to enjoy a grilled cheese while watching the waves - but don\'t forget to bring a sweater!';
+                    imageDesc.innerHTML = 'Taken at the San Francisco Exploratorium, this photo shows the excited sea on a windy afternoon. I love eating grilled cheese here while watching the waves - but it can get a little chilly!';
                     break;
                 default:
                     imageDesc.innerHTML = 'Select a photo to see what it\'s all about!';
